b13d6f0ffc7765d99a9afee89d7973b39b4cbe3b
Roberto Yoc <roberto.yoc@energyza.energy>
2020-03-05 11:57:14 -0600

    finished onetimepath

---
+++ onetime.py
@@ -0,0 +1,32 @@
+import random
+
+initial_key = "abcdefghijklmnopqrstuvwxyz "
+
+
+def change(shift, letter):
+    index = initial_key.index(letter) + shift
+    if index > 26:
+        index = index - 27
+    return initial_key[index]
+
+
+if __name__ == '__main__':
+    print('Ingresa tu texto')
+    text = input()
+    key = ""
+
+    for a in range(0,4):
+        key = key + random.choice(initial_key)
+
+    new_string = ""
+
+    for index in range(0, len(text)):
+        key_v = key[index%4]
+        shift = initial_key.index(key_v)
+        new_string = new_string + change(shift, text[index])
+
+    print("Llave: ")
+    print(key)
+
+    print("Texto Cifrado")
+    print(new_string)
